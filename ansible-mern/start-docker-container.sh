7e5039ab2edf8b69f1cd3e4ce767159c7a4b6480
Nikolai Limbrunner <ntli@kth.se>
2022-04-13 12:57:55 +0200

    Init environment

---
+++ ansible-mern/start-docker-container.sh
@@ -0,0 +1,13 @@
+docker pull ubuntu:14.04  # Target Container 
+docker pull williamyeh/ansible:ubuntu14.04-onbuild  # Ansibile Master
+
+docker network create mynetwork  
+
+mkdir shared_volume  
+cd shared_volume  
+
+docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume williamyeh/ansible:ubuntu14.04-onbuild /bin/bash -c 'while true; do sleep 60; echo keepalive; done'  
+docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  
+
+a() { docker exec -it ansible bash -c "echo 'PS1='\''ansible# '\' >> /root/.bashrc; bash"; 
+t() { docker exec -it target bash -c "echo 'PS1='\''target# '\' >> /root/.bashrc; bash"; }

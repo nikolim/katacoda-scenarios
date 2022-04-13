e393f804f931181264aa8c5901c0a6746b661932
Nikolai Limbrunner <ntli@kth.se>
2022-04-13 13:12:42 +0200

    Update env

--- ansible-mern/env-init.sh
+++ ansible-mern/env-init.sh
@@ -8,6 +8,3 @@ cd shared_volume
 
 docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume williamyeh/ansible:ubuntu14.04-onbuild /bin/bash -c 'while true; do sleep 60; echo keepalive; done'  
 docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  
-
-a() { docker exec -it ansible bash -c "echo 'PS1='\''ansible# '\' >> /root/.bashrc; bash"; 
-t() { docker exec -it target bash -c "echo 'PS1='\''target# '\' >> /root/.bashrc; bash"; }

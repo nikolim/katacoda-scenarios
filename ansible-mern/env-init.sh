a671ca9c028cb9c4c55b9b8543e10a161c345cb5
Nikolai Limbrunner <ntli@kth.se>
2022-04-13 17:05:51 +0200

    Pass systemctl to docker container

--- ansible-mern/env-init.sh
+++ ansible-mern/env-init.sh
@@ -1,4 +1,4 @@
-docker pull ubuntu:14.04  # Target Container 
+docker pull ubuntu:20.04  # Target Container 
 docker pull williamyeh/ansible:ubuntu14.04-onbuild  # Ansibile Master
 
 docker network create mynetwork  
@@ -7,4 +7,4 @@ mkdir shared_volume
 cd shared_volume  
 
 docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume williamyeh/ansible:ubuntu14.04-onbuild /bin/bash -c 'while true; do sleep 60; echo keepalive; done'  
-docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  
+docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume --privileged -v /run/systemd/system:/run/systemd/system -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket ubuntu:20.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  

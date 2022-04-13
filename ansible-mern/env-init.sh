3c419a7cb51490c92280a3d46fcc5ff9195e55b6
Nikolai Limbrunner <ntli@kth.se>
2022-04-13 17:47:47 +0200

    Remove priviledged commands

--- ansible-mern/env-init.sh
+++ ansible-mern/env-init.sh
@@ -7,4 +7,4 @@ mkdir shared_volume
 cd shared_volume  
 
 docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume williamyeh/ansible:ubuntu14.04-onbuild /bin/bash -c 'while true; do sleep 60; echo keepalive; done'  
-docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume --privileged -v /run/systemd/system:/run/systemd/system -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket ubuntu:20.04 -p 80:80 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  
+docker run -d --name target --rm --network mynetwork -p 80:80 -v $(pwd):/shared_volume ubuntu:20.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  
